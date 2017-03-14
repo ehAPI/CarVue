a6377d505ee85755a6075eb93d0f2aebb2d24205
surya muppalla <surya@anipr.in>
2017-03-14 15:43:51 +0400

    Configuration menu added

--- models/configuration.py
+++ models/configuration.py
@@ -15,10 +15,20 @@ class model(osv.osv):
 	_name = 'model'
 	_rec_name = 'model_of_car'
 	_columns = {
-		'make' : fields.many2one('make','Make',required=True,onchange='onchange_make(make)'),
+		'make' : fields.many2one('make','Make',required=True),
 		'model_of_car':fields.char('Model',size=50,required=True),
 		}
+
+	# def onchange_make(self,cr,uid,ids,customer,context=None):
+	# 	res={'value':{}}
+	# 	cust_obj = self.pool.get('make').browse(cr,uid,customer)
+	# 	res['value'].update({'sla_start_date':cust_obj.sla_start_date,
+	# 		'sla_end_date':cust_obj.sla_end_date,
+	# 		'country':cust_obj.country.id})
+
+
 model()
+
 
 class derivative(osv.osv):
 
--- models/vehicle_dashboard.py
+++ models/vehicle_dashboard.py
@@ -6,10 +6,10 @@ class vehicle_dashboard(osv.osv):
 
 	_name = 'vehicle.dashboard'
 	_columns = {
-		'registration' : fields.char('Registration',required=True),
-		'make' : fields.char('Make',required=True),
-		'model' : fields.char('Model',required=True),
-		'derivative' : fields.char('Derivative'),
+		'registration' : fields.char('Registration',required=True),	
+		'make' : fields.many2one('make','Make',required=True),
+		'model' : fields.many2one('model','Model',required=True, domain="[('make','=',make)]"),
+		'derivative' : fields.many2one('derivative','Derivative', domain="[('make','=',make)]"),
 		'age' : fields.integer('Age'),
 		'colour' : fields.char('Colour'),
 		'odometer' : fields.float('Odometer Reading'),
