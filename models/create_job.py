6ac3625c7110c60983040a9234e32bebc0b6a162
naha0102 <ansara@ehapi.com>
2017-04-02 14:49:29 +0400

    Added image for jobs menu item, edited the vehicle jobs button and so on

---
+++ models/create_job.py
@@ -0,0 +1,134 @@
+from openerp.osv import fields, osv
+import time 
+from datetime import datetime,timedelta
+from dateutil.relativedelta import relativedelta
+
+class create_job(osv.osv):
+
+	_name = 'create.job'
+	_rec_name='code'
+	_columns = {
+		# 'new':fields.char('New Job Card'),
+		# 'name':fields.one2many('res.partner','CONTACTS',ondelete='set null'),
+		'notes':fields.text('Notes'),
+		'code' : fields.char('Number',readonly=True),
+		'status':fields.selection([('prov','Provisional'),
+			('due','Due In')],'Status'),
+		# 'status':fields.selection([('prov','Provisional'),
+		# 	('due','Due In'),
+		# 	('arr','Arrived'),
+		# 	('in','In Progress'),
+		# 	('paused','Paused'),
+		# 	('part','Parts On Order'),
+		# 	('parts','Parts Arrived'),
+		# 	('awaiting','Awaiting Authority'),
+		# 	('cleaning','Cleaning'),
+		# 	('cust','Customer Contacted'),
+		# 	('work','Work Completed')],'STATUS'),
+		'due_in':fields.datetime('Due In'),
+		'due_out':fields.datetime('Due Out'),
+		'child_ids': fields.many2one('res.partner','Customer', domain=[('active','=',True)]), # force "active_test" domain to bypass _search() override
+		'advisor':fields.many2one('res.users','Advisor',ondelete='set null'),
+		'technician':fields.many2one('res.users','Technician',ondelete='set null'),
+		'veh':fields.many2one('vehicle.dashboard','Vehicle',ondelete='set null', domain="[('child_ids','=',child_ids)]"),
+		'bay' :fields.selection([('parking','Parking'),('ramp1','Ramp 1'),('ramp2','Ramp 2')],'Bay'),
+		'reference': fields.char('Reference'),
+		'mile':fields.integer('Mileage In'),
+		'image':fields.binary('Image',filters='*.png,*.gif'),
+	}
+	
+	def status_provisional(self,cr,uid,ids,context=None):
+		self.write(cr,uid,ids,{'status':'prov'},context=context)
+		return True
+
+	def status_duein(self,cr,uid,ids,context=None):
+		self.write(cr,uid,ids,{'status':'due'},context=context)
+		return True
+
+	def status_arr(self,cr,uid,ids,context=None):
+		self.write(cr,uid,ids,{'status':'arr'},context=context)
+		return True
+
+	def status_in(self,cr,uid,ids,context=None):
+		self.write(cr,uid,ids,{'status':'in'},context=context)
+		return True
+
+	def status_paused(self,cr,uid,ids,context=None):
+		self.write(cr,uid,ids,{'status':'paused'},context=context)
+		return True
+
+	def status_part(self,cr,uid,ids,context=None):
+		self.write(cr,uid,ids,{'status':'part'},context=context)
+		return True	
+
+	def status_parts(self,cr,uid,ids,context=None):
+		self.write(cr,uid,ids,{'status':'parts'},context=context)
+		return True
+
+	def status_awaiting(self,cr,uid,ids,context=None):
+		self.write(cr,uid,ids,{'status':'awaiting'},context=context)
+		return True	
+
+	def status_cleaning(self,cr,uid,ids,context=None):
+		self.write(cr,uid,ids,{'status':'cleaning'},context=context)
+		return True	
+
+	def status_cust(self,cr,uid,ids,context=None):
+		self.write(cr,uid,ids,{'status':'cust'},context=context)
+		return True	
+
+	def status_work(self,cr,uid,ids,context=None):
+		self.write(cr,uid,ids,{'status':'work'},context=context)
+		return True	
+
+	def create(self,cr,uid,vals,context=None):
+		if vals.get('code','/')=='/':
+			vals['code']=self.pool.get('ir.sequence').get(cr,uid,'create.job') or '/'
+		return super(create_job,self).create(cr,uid,vals,context=context)
+
+	def repairs_action(self, cr, uid, ids, context=None):
+		obj = self.browse(cr, uid, ids)
+
+		assert len(ids) == 1, 'This option should only be used for a single id at a time.'
+		
+		ctx = dict()
+		ctx.update({
+			'default_model': 'sale.order',
+			'default_partner_id': obj.child_ids.id,
+			'default_due_in' : obj.due_in,
+			'default_due_out' : obj.due_out,
+			'default_advisor' : obj.advisor.id,
+			'default_technician' : obj.technician.id,
+			'default_notes' : obj.notes,
+		})
+		return {
+			'type': 'ir.actions.act_window',
+			'view_type': 'form',
+			'view_mode': 'form',
+			'res_model': 'sale.order',
+			'context': ctx,
+		}
+
+	def on_change_due_in(self,cr,uid,ids,due_in,context=None):
+		duein = datetime.strptime(due_in,'%Y-%m-%d %H:%M:%S')
+		due_out=(duein + timedelta(hours=8)).strftime('%Y-%m-%d %H:%M:%S')
+		res={
+		'value' : {'due_out':due_out}
+		}
+		return res
+
+	# def on_change_customer(self,cr,uid,ids,customer,context=None):
+	# 	cust_image = self.pool.get('res.partner').read(cr,uid,customer,'image',context=context)
+	# 	# image = cust.image
+	# 	res={
+	# 	'value' : {'image':cust_image}
+	# 	}
+	# 	return res
+
+	_defaults={
+		'due_in': lambda *a:datetime.now().strftime('%Y-%m-%d %H:%M:%S'), 
+		'due_out': lambda *a:(datetime.now() + timedelta(hours=8)).strftime('%Y-%m-%d %H:%M:%S'),
+		'status':'prov',
+	}
+
+create_job()
---
+++ models/vehicle_dashboard.py
@@ -0,0 +1,80 @@
+from openerp.osv import fields, osv
+import datetime
+import time
+
+class vehicle_dashboard(osv.osv):
+
+	_name = 'vehicle.dashboard'
+	_rec_name = 'registration'
+	_columns = {
+		'code': fields.char('Code',readonly=True),
+		'registration' : fields.char('Registration',required=True),	
+		'make' : fields.many2one('make','Make',required=True),
+		'model' : fields.many2one('model','Model',required=True, domain="[('make','=',make)]"),
+		'derivative' : fields.many2one('derivative','Derivative', domain="[('model','=',model)]"),
+		'age' : fields.integer('Age'),
+		#'assign_to':fields.many2one('res.users','Assign To', required=True, domain="[('role','=','Surveyor')]"),
+		
+		'child_ids': fields.many2one('res.partner','Owner', domain=[('active','=',True)],required=True), #force "active_test" domain to bypass _search() override
+		'colour' : fields.char('Colour'),
+		'odometer' : fields.float('Odometer Reading'),
+		'odo_unit' : fields.selection([('miles','Miles'),('km','Kilometers')],'Odometer Unit'),
+		'model_year': fields.selection([(num, str(num)) for num in range(1901,(datetime.datetime.now().year)+1)], 'Model Year'),
+		'reg_date' : fields.date('Reg Date'),
+		'vin' : fields.char('VIN'),
+		'date_of_manufacture' : fields.char('Date of Manufacture'),
+		'paint_code' : fields.char('Paint Code'),
+		'trim' : fields.char('Trim'),
+		'fuel' : fields.selection([('petrol','Petrol'),
+									('diesel','Diesel'),
+									('electric','Electric'),
+									('hybrid','Hybrid'),
+									('lpg','LPG'),
+									('cng','CNG'),
+									('other','Other')],'Fuel'),
+		'door_plan' : fields.char('Door Plan'),
+		'gear_box' : fields.char('Gear Box'),
+		'gear_box_no' : fields.char('Gear Box No.'),
+		'co2emissions' : fields.char('CO2 Emissions'),
+		'engine_no' : fields.char('Engine No.'),
+		'engine_code' : fields.char('Engine Code'),
+		'engine_size' : fields.char('Engine Size'),
+		'power' : fields.char('Power'),
+		'key_no' : fields.char('Key No.'),
+		'radio_no' : fields.char('Radio No.'),
+		'mot_due_date' : fields.date('MOT Due Date'),
+		'service_due_date' : fields.date('Service Due Date'),
+		'warranty_ends' : fields.date('Warranty Ends'),
+		'notes' : fields.text('Notes'),
+		'contacts' : fields.char('Contacts'),
+		}
+
+	def create(self,cr,uid,vals,context=None):
+		if vals.get('code','/')=='/':
+			vals['code']=self.pool.get('ir.sequence').get(cr,uid,'vehicle.dashboard') or '/'
+		return super(vehicle_dashboard,self).create(cr,uid,vals,context=context)
+
+	def jobs_button(self, cr, uid, ids, context=None):
+		obj = self.browse(cr, uid, ids)
+		assert len(ids) == 1, 'This option should only be used for a single id at a time.'
+		return {
+			'type': 'ir.actions.act_window',
+			'view_mode': 'tree,kanban,form',
+			'domain':"[('veh.registration', '=',%s)]" %(obj.registration),
+			'res_model': 'create.job',
+		}
+
+		# job_tree = models_data._get_id(cr, uid, 'car_vue', 'view_create_job_tree')
+
+		# return {
+		# 	'name': 'Jobs',
+		# 	'view_type': 'form',
+		# 	"view_mode": 'tree,form',
+		# 	'res_model': 'create.job',
+		# 	'type': 'ir.actions.act_window',
+		# 	'search_view_id': job_tree,
+		# 	# 'res_id': res_ids,
+		# 	'domain':"[('id', 'in',%s)]" %(child_ids),
+		# }
+
+vehicle_dashboard()
